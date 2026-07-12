8863a9cd8aca0ac43e2bedf64f5e64e9beba57a0
Gabriel-FP <gabrielfelixpaim@gmail.com>
2026-07-12 05:31:30 -0300

    feat(infra): add pwa support with service worker and branded manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -6,6 +6,7 @@ import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const PgePreset = definePreset(Aura, {
   semantic: {
@@ -36,6 +37,9 @@ export const appConfig: ApplicationConfig = {
         preset: PgePreset,
         options: { darkModeSelector: '.pge-dark' },
       },
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
