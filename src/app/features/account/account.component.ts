db42c97c0d694e00c78dfa4f88cbda2cdb97cd10
Gabriel-FP <gabrielfelixpaim@gmail.com>
2026-07-12 00:50:16 -0300

    feat(shell): add bottom-nav shell with lazy tab routes

---
+++ src/app/features/account/account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  template: `
+    <h1>Account</h1>
+  `,
+  styles: ``
+})
+export class AccountComponent { }
